4eba15d95f2990b55b15a75780d7b784771a1296
Luo <604679490@qq.com>
2020-12-01 13:25:03 +0800

    修改了刷新成功加入购物车页面会继续增加购物车数量的bug

--- gmall-parent/web/web-all/src/main/java/com/atguigu/gmall/all/controller/CartController.java
+++ gmall-parent/web/web-all/src/main/java/com/atguigu/gmall/all/controller/CartController.java
@@ -1,14 +1,14 @@
 package com.atguigu.gmall.all.controller;
 
+import com.alibaba.fastjson.JSON;
 import com.atguigu.gmall.cart.feign.CartFeignClient;
-import com.atguigu.gmall.common.util.AuthContextHolder;
 import com.atguigu.gmall.model.cart.CartInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import javax.servlet.http.HttpServletRequest;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 /**
  * @author Miluo
@@ -20,11 +20,29 @@ public class CartController {
     private CartFeignClient cartFeignClient;
 
     @GetMapping("/addCart.html")
-    public String addCart(@RequestParam(name = "skuId") Long skuId, @RequestParam(name = "skuNum") Integer skuNum, HttpServletRequest request){
-        String userId = AuthContextHolder.getUserId(request);
-        String userTempId = AuthContextHolder.getUserTempId(request);
+    public String addCart(@RequestParam(name = "skuId") Long skuId, @RequestParam(name = "skuNum") Integer skuNum, RedirectAttributes attributes){
         CartInfo cartInfo = cartFeignClient.addCart(skuId,skuNum);
-        request.setAttribute("cartInfo",cartInfo);
+        String cartJson = JSON.toJSONString(cartInfo);
+        cartJson = cartJson.substring(1,cartJson.length()-1);
+        String[] split = cartJson.split(",");
+        int i = 0;
+        for (String s : split) {
+            if (i != 2){
+                String[] split1 = s.split(":",2);
+                attributes.addAttribute(split1[0].substring(1,split1[0].length()-1),split1[1]);
+            }else {
+                String[] split1 = s.split(":",2);
+                attributes.addAttribute(split1[0].substring(1,split1[0].length()-1),split1[1].substring(1,split1[1].length()-1));
+            }
+            i++;
+        }
+        return "redirect:http://cart.gmall.com/addToCart";
+    }
+
+    @GetMapping("/addToCart")
+    public String addToCart(CartInfo cartInfo, Model model){
+        System.out.println(cartInfo.toString());
+        model.addAttribute("cartInfo",cartInfo);
         return "cart/addCart";
     }
 }
