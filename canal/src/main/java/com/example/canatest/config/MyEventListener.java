c7e2fc856222ce4d2f83a08cf4b715d038fb1b32
Luo <604679490@qq.com>
2020-12-03 12:46:53 +0800

    修复了canal无法同步数据库的删除操作

--- canal/src/main/java/com/example/canatest/config/MyEventListener.java
+++ canal/src/main/java/com/example/canatest/config/MyEventListener.java
@@ -26,16 +26,35 @@ public class MyEventListener {
 //        rowData.getAfterColumnsList().forEach((c) -> System.err.println("By--Annotation: " + c.getName() + " ::   " + c.getValue()));
 //    }
 //
-//    @DeleteListenPoint
-//    public void onEvent3(CanalEntry.EventType eventType) {
-//        System.err.println("DeleteListenPoint");
-//    }
+    @DeleteListenPoint
+    public void onEvent3(CanalEntry.EventType eventType, CanalEntry.RowData rowData) {
+        System.out.println("有数据被删除了");
+        CartInfo cartInfo = new CartInfo();
+        StringBuilder sb = new StringBuilder();
+        rowData.getBeforeColumnsList().forEach((c) -> System.err.println("By--Annotation: " + c.getName() + " ::   " + c.getValue()));
+        rowData.getBeforeColumnsList().forEach((c) -> {sb.append(c.getValue()).append("::");
+        });
+        String[] attributes = sb.toString().split("::");
+        cartInfo.setId(Long.parseLong(attributes[0]));
+        cartInfo.setUserId(attributes[1]);
+        cartInfo.setSkuId(Long.parseLong(attributes[2]));
+        cartInfo.setCartPrice(new BigDecimal(attributes[3]));
+        cartInfo.setSkuNum(Integer.parseInt(attributes[4]));
+        cartInfo.setImgUrl(attributes[5]);
+        cartInfo.setSkuName(attributes[6]);
+        cartInfo.setIsChecked(Integer.parseInt(attributes[7]));
+        System.out.println("eventType:"+eventType);
+        System.out.println(cartInfo.toString());
+        redisTemplate.opsForHash().delete(RedisConst.USER_KEY_PREFIX + cartInfo.getUserId() + RedisConst.USER_CART_KEY_SUFFIX,cartInfo.getSkuId().toString());
+    }
+
+
     @Autowired
     private RedisTemplate redisTemplate;
 
     @ListenPoint(destination = "example", schema = "gmall_order", table = {"cart_info"}, eventType = {CanalEntry.EventType.UPDATE,CanalEntry.EventType.INSERT})
     public void onEvent4(CanalEntry.EventType eventType, CanalEntry.RowData rowData) {
-        System.err.println("DeleteListenPoint");
+
         CartInfo cartInfo = new CartInfo();
         StringBuilder sb = new StringBuilder();
         rowData.getAfterColumnsList().forEach((c) -> System.err.println("By--Annotation: " + c.getName() + " ::   " + c.getValue()));
@@ -51,7 +70,12 @@ public class MyEventListener {
         cartInfo.setImgUrl(attributes[5]);
         cartInfo.setSkuName(attributes[6]);
         cartInfo.setIsChecked(Integer.parseInt(attributes[7]));
+        System.out.println("eventType:"+eventType);
         System.out.println(cartInfo.toString());
-        redisTemplate.opsForHash().put(RedisConst.USER_KEY_PREFIX + cartInfo.getUserId() + RedisConst.USER_CART_KEY_SUFFIX,String.valueOf(cartInfo.getSkuId()),cartInfo);
+
+        if (eventType == CanalEntry.EventType.INSERT || eventType == CanalEntry.EventType.UPDATE){
+            redisTemplate.opsForHash().put(RedisConst.USER_KEY_PREFIX + cartInfo.getUserId() + RedisConst.USER_CART_KEY_SUFFIX,String.valueOf(cartInfo.getSkuId()),cartInfo);
+        }
+
     }
 }
